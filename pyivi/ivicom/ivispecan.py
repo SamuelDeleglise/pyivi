5a425641cf546f7cbf820e6ff464155493670c43
Scott McGimpsey <scott.mcgimpsey@aei.com>
2020-12-08 15:46:07 -0700

    Cast parent.traces.keys() to list as odict_keys are not indexable in Python 3+.

--- pyivi/ivicom/ivispecan.py
+++ pyivi/ivicom/ivispecan.py
@@ -158,7 +158,7 @@ class ShortCutSpecAn(ShortCut):
     def __init__(self, parent):
         super(ShortCutSpecAn, self).__init__(parent)
         self.trace_idx = 1
-        self.trace_idxs = Enum(['select trace'] + parent.traces.keys())
+        self.trace_idxs = Enum(['select trace'] + list(parent.traces.keys()))
         
     @property
     def frequency_center(self):
